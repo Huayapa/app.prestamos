1eb0fd85f7c00bfdd7b06ad85ec773e7cef54617
Huayapa <huayapajosue0@gmail.com>
2025-11-10 09:35:34 -0500

    models tables

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'books';
+    protected $fillable = [
+        'title',
+        'summary',
+        'author',
+        'isbn',
+        'publication_year',
+        'stock'
+    ];
+
+    public function loans()
+    {
+        return $this->hasMany(Loan::class);
+    }
 }
--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Loan extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'loans';
+    protected $fillable = [
+        'borrower_id',
+        'student_id',
+        'book_id',
+        'due_date',
+        'return_date',
+        'load_status'
+    ];
+
+    public function borrower() {
+        return $this->belongsTo(User::class, 'borrower_id');
+    }
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
 }
